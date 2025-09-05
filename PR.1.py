9fa710b7415a3f6e48249330c9bd061aa15fa061
kajalharijan07-cloud <kajalharijan07@gmail.com>
2025-09-05 13:37:16 +0530

    Add files via upload

---
+++ PR.1.py
@@ -0,0 +1,13 @@
+
+
+
+
+name=str(input("enter your name:"))
+age=int(input("enter your age:"))
+height=float(input("enter your height:"))
+favourite_color=input("enter your favourite color:")
+print(name,age,height,favourite_color) 
+print(type(name),type(age),type(height),type(favourite_color))
+id=input("enter your id:")
+course="python"
+print(id,course)
