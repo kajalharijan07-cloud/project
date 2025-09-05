966414f7e88994b7bc2112789f8f3c66068494f2
kajalharijan07-cloud <kajalharijan07@gmail.com>
2025-09-05 13:56:40 +0530

    Add files via upload

---
+++ PR.1.py
@@ -0,0 +1,9 @@
+print("Welcome to the Interactive Personal Data Collector")
+name=str(input("enter your name:"))
+age=int(input("enter your age:"))
+height=float(input("enter your height in metres:"))
+favourite_number=int(input("enter your favourite number:"))
+print(name,type(name),id(name),age,type(age),id(age),height,type(height),id(height),favourite_number,type(favourite_number),id(favourite_number))
+Birth_Year = 2025-age
+print(Birth_Year)
+print("Thank you for using the Interactive Personal Data Collector. Goodbye!")
